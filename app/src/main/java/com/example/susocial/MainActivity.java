fa66712fea9467df33ad9eddc8ad480e9bff0774
Eric Rodriguez <erodri07@syr.edu>
2023-02-14 22:31:32 -0500

    Made a Push notifications button in the Profile UI page.

--- app/src/main/java/com/example/susocial/MainActivity.java
+++ app/src/main/java/com/example/susocial/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.susocial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button profileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Buttons
+        profileButton = findViewById(R.id.profileButton);
+        profileButton.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.profileButton:
+                openProfileActivity();
+                break;
+        }
+    }
+
+    private void openProfileActivity() {
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/susocial/Profile.java
@@ -0,0 +1,87 @@
+package com.example.susocial;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Profile extends AppCompatActivity implements View.OnClickListener {
+    //areNotificationsEnabled()
+    private final int NOTIFICATION_PERMISSION_CODE = 1;
+    private Button requestNotiButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        requestNotiButton = findViewById(R.id.RequestNotifications);
+        requestNotiButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.RequestNotifications:
+                //request Notifications from User
+                if (ContextCompat.checkSelfPermission(Profile.this, 
+                        Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(Profile.this, "Notification Permissions Granted", 
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    requestNotiPermission();
+                }
+                break;
+        }
+    }
+
+    private void requestNotiPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.POST_NOTIFICATIONS)) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("PermissionNeeded")
+                    .setMessage("Permission needed for Notifications")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(Profile.this,
+                                    new String[] {Manifest.permission.POST_NOTIFICATIONS}, NOTIFICATION_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.POST_NOTIFICATIONS}, NOTIFICATION_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == NOTIFICATION_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Notifications Will Now Be Pushed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}

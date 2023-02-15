88bdbb3be32a2f55825e210751c5f45fe5ab32be
Mingyan Zhang <mzhang78@syr.edu>
2023-02-15 17:33:31 -0500

    test

--- app/src/main/java/com/example/susocial/Profile.java
+++ app/src/main/java/com/example/susocial/Profile.java
@@ -27,7 +27,7 @@ public class Profile extends AppCompatActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        //hi
         requestNotiButton = findViewById(R.id.RequestNotifications);
         requestNotiButton.setOnClickListener(this);
 

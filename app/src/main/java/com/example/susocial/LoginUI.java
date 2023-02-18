55774d25d28c223f7d3cc44579ad69d191e2d6b2
Melissa <mltang@syr.com>
2023-02-17 21:31:47 -0500

    Login UI

---
+++ app/src/main/java/com/example/susocial/LoginUI.java
@@ -0,0 +1,22 @@
+package com.example.susocial;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class LoginUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_ui);
+    }
+
+
+}

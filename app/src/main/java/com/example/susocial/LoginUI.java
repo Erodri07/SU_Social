72605737cf2c049fa0215425b4d43742a3bad983
Melissa <mltang@syr.com>
2023-02-18 14:57:01 -0500

    Login UI + login fail pop-up

--- app/src/main/java/com/example/susocial/LoginUI.java
+++ app/src/main/java/com/example/susocial/LoginUI.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class LoginUI extends AppCompatActivity {
@@ -16,6 +17,10 @@ public class LoginUI extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_ui);
+
+        //popup text for if user fails
+        //if(login fails) then display this message
+        Toast.makeText(LoginUI.this, "Login Failed", Toast.LENGTH_SHORT).show();
     }
 
 

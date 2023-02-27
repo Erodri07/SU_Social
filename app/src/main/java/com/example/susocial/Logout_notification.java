a8b964203f68e24f6e43dbb5da7555e23b416ab5
Alexa <alexander4battle@gmail.com>
2023-02-26 22:17:46 -0500

    This code is for the logout notification.

---
+++ app/src/main/java/com/example/susocial/Logout_notification.java
@@ -0,0 +1,66 @@
+package com.example.susocial;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class Logout_notification extends AppCompatActivity {
+
+    Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        logout = findViewById(R.id.logout);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("Logout", "Lougout notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //notification code here
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(Logout_notification.this, "Logout Notification");
+                builder.setContentTitle("Logout");
+                builder.setContentText("You have logged out successfully");
+                builder.setSmallIcon(R.drawable.ic_launcher_background);
+                builder.setAutoCancel(true);
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(Logout_notification.this);
+                if (ActivityCompat.checkSelfPermission(Logout_notification.this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                managerCompat.notify(1, builder.build());
+
+
+
+            }
+        });
+
+
+
+    }
+
+}

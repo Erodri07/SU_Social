934adacc83ee2bd79bb3529e5d847151b8775eca
Mingyan Zhang <mzhang78@syr.edu>
2023-03-04 17:47:45 -0500

    Finish Account deleting confirmation popup window

--- app/src/main/java/com/example/susocial/Profile.java
+++ app/src/main/java/com/example/susocial/Profile.java
@@ -8,12 +8,16 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -25,8 +29,11 @@ public class Profile extends AppCompatActivity implements View.OnClickListener {
     private final int NOTIFICATION_PERMISSION_CODE = 1;
     private Button requestNotiButton;
     private Button logoutButton;
-
     private Button editProfButton;
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog myDialog;
+    private Button btndeleteCancel,btndeleteYes;
 
 
     @Override
@@ -46,6 +53,56 @@ public class Profile extends AppCompatActivity implements View.OnClickListener {
 
         editProfButton = findViewById(R.id.EditProfile);
         editProfButton.setOnClickListener(this);
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.setting_menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.settingItem1:
+                startActivity(new Intent(Profile.this,edit_profile.class));
+                return true;
+            case R.id.settingItem3:
+                deleteConfirmationDialog();
+                return true;
+            case R.id.settingItem4:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Profile.this, LoginUI.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    // Delete Account Confiramtion Popup window
+    public void deleteConfirmationDialog(){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View popupView = getLayoutInflater().inflate(R.layout.deleteaccount_confirmation,null);
+
+        btndeleteCancel = (Button)popupView.findViewById(R.id.delete_cancel);
+        btndeleteYes = (Button) popupView.findViewById(R.id.delete_delete);
+
+        dialogBuilder.setView(popupView);
+        myDialog = dialogBuilder.create();
+        myDialog.show();
+
+        btndeleteYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Profile.this, LoginUI.class));
+            }
+        });
+        btndeleteCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
 
     }
 

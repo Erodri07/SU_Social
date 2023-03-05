e2d5bb173e463cbc4ff239cfbcfeb23c2a0f48a5
Melissa <mltang@syr.com>
2023-03-04 23:02:37 -0500

    Password reset form + Profile UI's

---
+++ app/src/main/java/com/example/susocial/passwordRetrieval.java
@@ -0,0 +1,14 @@
+package com.example.susocial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class passwordRetrieval extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_retrieval);
+    }
+}

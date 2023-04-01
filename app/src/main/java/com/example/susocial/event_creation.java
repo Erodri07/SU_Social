02359964cb725deffa5c0d0188dee5a5432757a5
Eric Rodriguez <erodri07@syr.edu>
2023-04-01 19:19:08 -0400

    Added a timer to events and restricted date so only valid dates are entered

--- app/src/main/java/com/example/susocial/event_creation.java
+++ app/src/main/java/com/example/susocial/event_creation.java
@@ -6,18 +6,23 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -32,15 +37,21 @@ import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Calendar;
 
 public class event_creation extends AppCompatActivity implements View.OnClickListener{
 
-    private EditText eventDate, eventTime, eventLocation, eventDescription;
-    private TextView eventName;
+    private EditText eventDate, eventLocation, eventDescription;
+    private TextView eventName, eventTime;
     private Button cancelButton, submitButton, addEventPic;
     private ImageView eventPicture;
     private FirebaseFirestore db;
     private StorageReference imageStorageRef;
+    private TimePickerDialog timePickerDialog;
+    private Calendar calendarTime;
+    private int currentHour;
+    private int currentMinute;
+    private String amPm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +60,9 @@ public class event_creation extends AppCompatActivity implements View.OnClickLis
 
         eventName = findViewById(R.id.eventCreationName);
         eventDate = findViewById(R.id.eventCreationDate);
+        eventDate.addTextChangedListener(tw);
         eventTime = findViewById(R.id.eventCreationTime);
+        eventTime.setOnClickListener(this);
         eventLocation = findViewById(R.id.eventCreationLocation);
         eventDescription = findViewById(R.id.eventCreationDescription);
 
@@ -80,10 +93,95 @@ public class event_creation extends AppCompatActivity implements View.OnClickLis
             case R.id.addProfileImageButton:
                 uploadPicture();
                 break;
+            case R.id.eventCreationTime:
+                selectTime();
+                break;
             case R.id.eventCreationCancel:
                 startActivity(new Intent(event_creation.this, Home.class));
         }
     }
+
+    private void selectTime() {
+        calendarTime = Calendar.getInstance();
+        currentHour = calendarTime.get(Calendar.HOUR_OF_DAY);
+        currentMinute = calendarTime.get(Calendar.MINUTE);
+
+        timePickerDialog = new TimePickerDialog(event_creation.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                if (hourOfDay >= 12) {
+                    amPm = "PM";
+                } else {
+                    amPm = "AM";
+                }
+                eventTime.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
+            }
+        }, currentHour, currentMinute, false);
+
+        timePickerDialog.show();
+    }
+
+    TextWatcher tw = new TextWatcher() {
+        private String current = "";
+        private String ddmmyyyy = "DDMMYYYY";
+        private Calendar cal = Calendar.getInstance();
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (!s.toString().equals(current)) {
+                String clean = s.toString().replaceAll("[^\\d.]|\\.", "");
+                String cleanC = current.replaceAll("[^\\d.]|\\.", "");
+
+                int cl = clean.length();
+                int sel = cl;
+                for (int i = 2; i <= cl && i < 6; i += 2) {
+                    sel++;
+                }
+                //Fix for pressing delete next to a forward slash
+                if (clean.equals(cleanC)) sel--;
+
+                if (clean.length() < 8){
+                    clean = clean + ddmmyyyy.substring(clean.length());
+                }else{
+                    //This part makes sure that when we finish entering numbers
+                    //the date is correct, fixing it otherwise
+                    int day  = Integer.parseInt(clean.substring(0,2));
+                    int mon  = Integer.parseInt(clean.substring(2,4));
+                    int year = Integer.parseInt(clean.substring(4,8));
+
+                    mon = mon < 1 ? 1 : mon > 12 ? 12 : mon;
+                    cal.set(Calendar.MONTH, mon-1);
+                    year = (year<1900)?1900:(year>2100)?2100:year;
+                    cal.set(Calendar.YEAR, year);
+                    // ^ first set year for the line below to work correctly
+                    //with leap years - otherwise, date e.g. 29/02/2012
+                    //would be automatically corrected to 28/02/2012
+
+                    day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                    clean = String.format("%02d%02d%02d",day, mon, year);
+                }
+
+                clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                        clean.substring(2, 4),
+                        clean.substring(4, 8));
+
+                sel = sel < 0 ? 0 : sel;
+                current = clean;
+                eventDate.setText(current);
+                eventDate.setSelection(sel < current.length() ? sel : current.length());
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 
     private void uploadPicture() {
         Intent intent = new Intent();
@@ -152,7 +250,7 @@ public class event_creation extends AppCompatActivity implements View.OnClickLis
         String date = eventDate.getText().toString();
         String time = eventTime.getText().toString();
         String location = eventLocation.getText().toString();
-        String description = eventDate.getText().toString();
+        String description = eventDescription.getText().toString();
 
         if (TextUtils.isEmpty(name)) {
             eventName.setError("Event Name cannot be Empty");
